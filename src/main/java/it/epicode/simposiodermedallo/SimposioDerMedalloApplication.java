a8dc0d098ac04ca9a49f5412b6e7a6a409729017
MeridjanShoti <meridjanshoti@gmail.com>
2025-05-10 16:57:12 +0200

    Initial commit

---
+++ src/main/java/it/epicode/simposiodermedallo/SimposioDerMedalloApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.simposiodermedallo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimposioDerMedalloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimposioDerMedalloApplication.class, args);
+	}
+
+}
