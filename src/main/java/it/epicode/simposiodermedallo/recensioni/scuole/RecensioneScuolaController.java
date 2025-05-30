a5385ca8c3a41ff6c45ed226a5cdc20803ff1a5f
MeridjanShoti <meridjanshoti@gmail.com>
2025-05-30 18:21:44 +0200

    recensioni scuole

--- src/main/java/it/epicode/simposiodermedallo/recensioni/scuole/RecensioneScuolaController.java
+++ src/main/java/it/epicode/simposiodermedallo/recensioni/scuole/RecensioneScuolaController.java
@@ -1,4 +1,47 @@
 package it.epicode.simposiodermedallo.recensioni.scuole;
 
+import it.epicode.simposiodermedallo.auth.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/recensioni-scuola")
 public class RecensioneScuolaController {
+    @Autowired
+    private RecensioneScuolaService recensioneScuolaService;
+    @GetMapping("/scuole/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("isAuthenticated()")
+    public Page<RecensioneScuola> getRecensioniScuola(@PathVariable Long id, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sort, @RequestParam(defaultValue = "asc") String sortDir) {
+        return recensioneScuolaService.getAll(id, page, size, sort, sortDir);
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("isAuthenticated()")
+    public RecensioneScuola getRecensioneScuola(@PathVariable Long id) {
+        return recensioneScuolaService.getRecensioneScuola(id);
+    }
+    @PostMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("isAuthenticated()")
+    public RecensioneScuola creaRecensioneSala(@PathVariable Long id, @RequestBody RecensioneScuolaRequest request, @AuthenticationPrincipal AppUser user) {
+        return recensioneScuolaService.save(id, request, user);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("isAuthenticated()")
+    public void deleteRecensioneScuola(@PathVariable Long id, @AuthenticationPrincipal AppUser user) {
+        recensioneScuolaService.deleteRecensioneScuola(id, user);
+    }
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("isAuthenticated()")
+    public RecensioneScuola updateRecensioneSala(@PathVariable Long id, @RequestBody RecensioneScuolaRequest request, @AuthenticationPrincipal AppUser user) {
+        return recensioneScuolaService.update(id, request, user);
+    }
 }
--- src/main/java/it/epicode/simposiodermedallo/recensioni/scuole/RecensioneScuolaRepository.java
+++ src/main/java/it/epicode/simposiodermedallo/recensioni/scuole/RecensioneScuolaRepository.java
@@ -1,10 +1,12 @@
 package it.epicode.simposiodermedallo.recensioni.scuole;
 
 
+import it.epicode.simposiodermedallo.recensioni.sale.RecensioneSala;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecensioneScuolaRepository extends JpaRepository<RecensioneScuola, Long> {
     Page<RecensioneScuola> findAllByScuolaId(Long id, Pageable pageable);
+    Boolean existsByScuolaIdAndAutoreId(Long salaId, Long autoreId);
 }
