2540d70f7f9247feb794b640e2ac2e4a0af96b63
MeridjanShoti <meridjanshoti@gmail.com>
2025-06-01 19:56:43 +0200

    controller iscrizioni

--- src/main/java/it/epicode/simposiodermedallo/utenti/servizi/scuole/corsi/iscrizioni/IscrizioneRepository.java
+++ src/main/java/it/epicode/simposiodermedallo/utenti/servizi/scuole/corsi/iscrizioni/IscrizioneRepository.java
@@ -1,8 +1,13 @@
 package it.epicode.simposiodermedallo.utenti.servizi.scuole.corsi.iscrizioni;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IscrizioneRepository extends JpaRepository<Iscrizione, Long> {
 boolean existsByUtenteIdAndCorsoId(Long utenteId, Long corsoId);
+Iscrizione findByUtenteIdAndCorsoId(Long utenteId, Long corsoId);
+Page<Iscrizione> findAllByCorsoId(Long corsoId, Pageable pageable);
+Page<Iscrizione> findAllByUtenteId(Long utenteId, Pageable pageable);
 }
---
+++ src/main/java/it/epicode/simposiodermedallo/utenti/servizi/scuole/corsi/iscrizioni/VotoRequest.java
@@ -0,0 +1,8 @@
+package it.epicode.simposiodermedallo.utenti.servizi.scuole.corsi.iscrizioni;
+
+import lombok.Data;
+
+@Data
+public class VotoRequest {
+    private Double valutazione;
+}
